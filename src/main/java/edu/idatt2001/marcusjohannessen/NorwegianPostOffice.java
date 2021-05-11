0c10e2b2f1a1a8dfb6cb5ac9eefee9adf24d1dc8
Marcus Johannessen <marcusjo@stud.ntnu.no>
2021-05-11 22:53:58 +0200

    Added NorwegianPostOffice, maybe PostOffice should be interface?

---
+++ src/main/java/edu/idatt2001/marcusjohannessen/NorwegianPostOffice.java
@@ -0,0 +1,9 @@
+package edu.idatt2001.marcusjohannessen;
+
+
+public class NorwegianPostOffice extends PostOffice {
+
+    public NorwegianPostOffice(String zipCode, String municipality, String city) {
+        super(zipCode, municipality, city);
+    }
+}
