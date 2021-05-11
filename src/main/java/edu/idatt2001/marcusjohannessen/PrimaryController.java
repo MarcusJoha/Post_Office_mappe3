05514e171b36749e72eab11bf1a46e1ac62300f0
Marcus Johannessen <marcusjo@stud.ntnu.no>
2021-05-11 19:59:46 +0200

    Final layout, gave up that fucking side slider mernu

--- src/main/java/edu/idatt2001/marcusjohannessen/PrimaryController.java
+++ src/main/java/edu/idatt2001/marcusjohannessen/PrimaryController.java
@@ -1,20 +1,31 @@
 package edu.idatt2001.marcusjohannessen;
 
-import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
+import javafx.animation.TranslateTransition;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class PrimaryController {
+public class PrimaryController implements Initializable {
 
+    @FXML
+    private BorderPane borderPane;
     @FXML
     private TableView<PostOffice> tableView;
     @FXML
@@ -26,7 +37,17 @@ public class PrimaryController {
     @FXML
     private TextField filterfield;
     @FXML
-    private Label amount;
+    private Label amountField;
+    @FXML
+    private VBox slider;
+    @FXML
+    private Label menu;
+
+    @FXML
+    private Label menuBack;
+
+
+
 
 
     //PostOfficeRegister postOfficeRegister = new PostOfficeRegister();
@@ -36,8 +57,8 @@ public class PrimaryController {
      * Initializes when the application is started.
      */
 
-    @FXML
-    public void initialize(){
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
         setCellProperty();
         try {
             filehandler.readFromFile();
@@ -47,14 +68,12 @@ public class PrimaryController {
         FilteredList<PostOffice> filteredList = new FilteredList<>(filehandler.getPostOffices(), f -> true );
 
         //Filters tableview based on zipcode and municipality
-        filterfield.textProperty().addListener((observableValue, obVal, newVal) -> {
+        filterfield.textProperty().addListener((observableValue, oldVal, newVal) -> {
             filteredList.setPredicate(PostOffice -> {
                 if (newVal == null || newVal.isEmpty()) {
                     return true;
                 }
-
                 String lowerCaseFilter = newVal.toLowerCase();
-
                 if (PostOffice.getZipCode().toLowerCase().contains(lowerCaseFilter)){
                     return true;
                 }
@@ -64,11 +83,13 @@ public class PrimaryController {
 
         SortedList<PostOffice> sortedList = new SortedList<>(filteredList);
         sortedList.comparatorProperty().bind(tableView.comparatorProperty());
-        //
         tableView.setItems(sortedList);
+    }
 
-        //ToDo: få denne til å forandre seg.
-        amount.setText("Amount: " + sortedList.size());
+
+    //TODO: Er ikke viktig, sparer den til senere
+    private void getSizeOfTableview(){
+
     }
 
     private void setCellProperty() {
@@ -76,13 +97,6 @@ public class PrimaryController {
         municipalityCol.setCellValueFactory(new PropertyValueFactory<>("municipality"));
         cityCol.setCellValueFactory(new PropertyValueFactory<>("city"));
     }
-
-    public void setAmount(){
-        amount.setText("Amount: " + tableView.getItems().size());
-
-    }
-
-
 
     //TODO: tableview oppdaterer seg mens brukeren skriver
     //Når man trykker enter søker man
